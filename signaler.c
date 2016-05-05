293e7c60475910b688e2ee162c453247307aae29
pauliracane <smallmaddog11@yahoo.com>
2016-05-05 14:28:14 -0400

    Changed number from UINT_MAX to SHRT_MAX for sake of testing.  Added in prime number generator / Printing.

--- signaler.c
+++ signaler.c
@@ -5,44 +5,50 @@
 #include <time.h>	//You don't want to know.
 
 
-#define SIGHUP 1
-#define SIGUSR1 10
-#define SIGUSR2 12
-
 void SIGHUP_Handler(int signum);
 void SIGUSR1_Handler(int signum);
 void SIGUSR2_Handler(int signum);
+int * GenPrimes(unsigned int *a);
+
 
 int main(int argc, char *argv[])
 {
-	unsigned int i = 0, j = 0;		//Loop Control Variables
-
-	int total_time = time.time();
+	int total_time = time(NULL);
 
 	printf("Attempting to malloc array.\n");
 
-	unsigned int *a = calloc(UINT_MAX, sizeof *a); //Array for Numbers
+	unsigned int *a = calloc(SHRT_MAX, sizeof *a); //Array for Numbers
 	printf("Starting!\n");
-	for(j=0,i=2; i < UINT_MAX && i > 0; i++, j++)
+
+	// http://stackoverflow.com/a/31322918
+	for(unsigned int j=0,i=2; i < SHRT_MAX && i > 0; i++, j++)
 	{
 		a[j]=i;	
-		if(i%10000 == 0)
-		{
-			printf("%u\n", i);
-		}
 	}
-	printf("Finished.");
 
+    printf("Starting Prime Number Gen.\n");
+	printf("Time taken: %ld\n", time(NULL) - total_time);
 
+	a = GenPrimes(a);
 
-	signal(SIGHUP, SIGHUP_Handler);
-	signal(SIGUSR1, SIGUSR1_Handler);
-	signal(SIGUSR2, SIGUSR2_Handler);
-	
+	printf("Finished in %ld.\n", time(NULL) - total_time);
+
+	for(unsigned int i = 0; i < SHRT_MAX; i++) 
+	{
+		/* Print all the non Zero *Prime numbers* */
+		if(a[i] != 0) 
+		{
+			printf("%d \n", a[i]);
+		}
+
+		signal(SIGHUP, SIGHUP_Handler);
+		signal(SIGUSR1, SIGUSR1_Handler);
+		signal(SIGUSR2, SIGUSR2_Handler);
+	}
 	free(a);
 	return 0;
 }
-	
+
 void SIGHUP_Handler(int signum)
 {
 	printf("%d\n", signum);
@@ -54,4 +60,26 @@ void SIGUSR1_Handler(int signum)
 void SIGUSR2_Handler(int signum)
 {
     printf("%d\n", signum);
+}
+
+int *GenPrimes(unsigned int *a)
+{
+	for(unsigned int i = 0; i < SHRT_MAX; i++)
+    {
+        int num = a[i];
+
+		//If number is not 0, update later array index.
+		if ( num != 0 )
+		{
+			for (unsigned int j = i+1; j < SHRT_MAX; j++)
+			{
+				if( ( a[j]%num == 0 ) )
+				{
+					a[j] = 0;
+				}
+			}
+		}
+	}
+
+	return a;
 }
